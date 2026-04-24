4ea344120ae3996dd4bcd36ca0c560c05878fba5
cris <xcc137396549@foxmail.com>
2026-04-24 12:18:09 +0800

    [IMP] 优化示例工程

--- mybatisgx-example-spring-boot3/src/main/java/com/mybatisgx/example/spring/boot/entity/RoleQuery.java
+++
@@ -1,9 +0,0 @@
-package com.mybatisgx.example.spring.boot.entity;
-
-import com.mybatisgx.annotation.QueryEntity;
-import lombok.Data;
-
-@Data
-@QueryEntity(Role.class)
-public class RoleQuery extends Role {
-}
--- mybatisgx-example-spring-boot4/src/main/java/com/mybatisgx/example/spring/boot/entity/MenuQuery.java
+++
@@ -1,9 +0,0 @@
-package com.mybatisgx.example.spring.boot.entity;
-
-import com.mybatisgx.annotation.QueryEntity;
-import lombok.Data;
-
-@Data
-@QueryEntity(Menu.class)
-public class MenuQuery extends Menu {
-}
